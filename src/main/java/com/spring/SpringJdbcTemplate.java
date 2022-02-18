282fb29ba14ff616e1f7084c2716f017bf8335ab
Amrutha <amruthaedu665@gmail.com>
2022-02-18 15:39:52 +0530

    spring jdbc template,named template

---
+++ src/main/java/com/spring/SpringJdbcTemplate.java
@@ -0,0 +1,24 @@
+package com.spring;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class SpringJdbcTemplate {
+JdbcTemplate jt;
+
+public void setJt(JdbcTemplate jt) {
+	this.jt = jt;
+}
+
+public void select() {
+	
+	List list = jt.queryForList("select * from student");
+	Iterator itr = list.iterator();
+	while(itr.hasNext()) {
+		System.out.println(itr.next());
+	}
+	
+	}
+}
